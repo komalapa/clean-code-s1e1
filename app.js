d724a68d2ac28c57d92207dc20b719e2c5f839a7
komalapa <komalapa@gmail.com>
2021-11-25 19:37:32 +0400

    refactor: no tag selectors in js  according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -90,8 +90,8 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var editInput=listItem.querySelector('.task-input[type=text]');
+    var label=listItem.querySelector(".task");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
